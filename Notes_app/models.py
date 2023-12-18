3faa4829c28ea0509155d7ca8a922aa72e30cd5c
AbdurrohmanJCom <tuckerticket@gmail.com>
2023-12-18 16:39:07 +0500

    templates, urls, views, models | added

--- Notes_app/models.py
+++ Notes_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.note.title}"
